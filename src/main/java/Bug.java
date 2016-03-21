a443a1e339b634773da9f05576f949dc9fb04cb5
Kay Roepke <kroepke@googlemail.com>
2016-03-21 09:31:02 +0100

    initial import for reproducible bug test case

---
+++ src/main/java/Bug.java
@@ -0,0 +1,27 @@
+import com.google.inject.TypeLiteral;
+
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import static java.util.stream.Collectors.toMap;
+
+public class Bug {
+
+    public static void main(String[] args) {
+
+        new Bug().run();
+    }
+
+    private void run() {
+        final ParameterDescriptor<Map<String, String>, Map<String, Pattern>> pathsParam;
+
+        //noinspection unchecked
+        pathsParam = ParameterDescriptor.type("paths",
+                                              (Class<? extends Map<String, String>>) new TypeLiteral<Map<String, String>>() {}.getRawType(),
+                                              (Class<? extends Map<String, Pattern>>) new TypeLiteral<Map<String, Pattern>>() {}.getRawType())
+                .transform(inputMap -> inputMap.entrySet().stream()
+                        .collect(toMap(Map.Entry::getKey, e -> Pattern.compile(e.getValue()))))
+                .build();
+    }
+
+}
